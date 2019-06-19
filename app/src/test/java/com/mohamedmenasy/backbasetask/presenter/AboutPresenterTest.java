009cbfdee1a11330d521070f6f0ca0ea05330ad5
Mohamed Nabil <mohamedmenasy@gmail.com>
2019-06-19 02:13:43 +0200

    about unit test

--- app/src/test/java/com/mohamedmenasy/backbasetask/presenter/AboutPresenterTest.java
+++ app/src/test/java/com/mohamedmenasy/backbasetask/presenter/AboutPresenterTest.java
@@ -1,6 +1,9 @@
 package com.mohamedmenasy.backbasetask.presenter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.os.Handler;
+import android.os.Message;
 
 import com.mohamedmenasy.backbasetask.features.about.About;
 import com.mohamedmenasy.backbasetask.features.about.AboutInfo;
@@ -9,12 +12,26 @@ import com.mohamedmenasy.backbasetask.features.about.AboutPresenterImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static android.os.Looper.getMainLooper;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(MockitoJUnitRunner.class)
@@ -25,7 +42,7 @@ public class AboutPresenterTest {
     @Mock
     About.View view;
     private AboutPresenterImpl presenter;
-    
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
